57633f56fcff01ac55e039a78265b589a091e328
Jim Blackler <jimblackler@google.com>
2019-02-11 10:25:42 +0000

    Tidy shaders

--- v2/scripts/script.js
+++ v2/scripts/script.js
@@ -48,32 +48,29 @@ function init() {
 
   // SKYDOME
 
+  //language=GLSL
   const vertexShader = `
     varying vec3 vWorldPosition;
-  
+    
     void main() {
-  
-      vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
+      vec4 worldPosition = modelMatrix * vec4(position, 1.0);
       vWorldPosition = worldPosition.xyz;
-  
-      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-  
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
     }
   `;
 
+  //language=GLSL
   const fragmentShader = `
     uniform vec3 topColor;
     uniform vec3 bottomColor;
     uniform float offset;
     uniform float exponent;
-
+    
     varying vec3 vWorldPosition;
-
+    
     void main() {
-
-      float h = normalize( vWorldPosition + offset ).y;
-      gl_FragColor = vec4( mix( bottomColor, topColor, max( pow( max( h, 0.0 ), exponent ), 0.0 ) ), 1.0 );
-
+      float h = normalize(vWorldPosition + offset).y;
+      gl_FragColor = vec4(mix(bottomColor, topColor, max(pow(max(h, 0.0), exponent), 0.0)), 1.0);
     }
   `;
   const uniforms = {
