d3887089fff0457ab5d5de4ef84c9f5b088583ae
Jim Blackler <jimblackler@google.com>
2019-02-24 16:00:05 +0000

    First rendering of playing card. Scene updates.

--- v2/scripts/script.js
+++ v2/scripts/script.js
@@ -2,6 +2,7 @@ import {ObjectLoader} from "../threejs/src/loaders/ObjectLoader.js";
 import {OrbitControls} from "../threejs/controls/OrbitControls.js";
 import {PCFSoftShadowMap} from "../threejs/src/constants.js";
 import {Scene} from "../threejs/src/scenes/Scene.js";
+import {Vector2} from "../threejs/src/math/Vector2.js";
 import {WebGLRenderer} from "../threejs/src/renderers/WebGLRenderer.js";
 
 const SCREEN_WIDTH = window.innerWidth;
@@ -24,13 +25,30 @@ container.appendChild(renderer.domElement);
 renderer.gammaInput = true;
 renderer.gammaOutput = true;
 
+const CARD_WIDTH = 103; // TODO: unify with classic renderer
+const CARD_HEIGHT = 143; // TODO: unify with classic renderer
+const TEXTURE_WIDTH = 1339;
+const TEXTURE_HEIGHT = 900;
+
 const loader = new ObjectLoader();
 loader.load('data/app.json', object => {
   scene.add(object);
+  const card = scene.getObjectByName('Card');
+  const front = card.getObjectByName('Front');
+
+  const suit = 3;
+  const type = 12;
+
+
+  const map = front.material.map;
+  map.repeat = new Vector2(CARD_WIDTH / TEXTURE_WIDTH, CARD_HEIGHT / TEXTURE_HEIGHT);
+  map.offset = new Vector2((CARD_WIDTH * type) / TEXTURE_WIDTH, 1 - (CARD_HEIGHT * suit + CARD_HEIGHT) /  TEXTURE_HEIGHT);
+
   loader.load('data/camera.json', camera => {
     scene.add(camera);
     const controls = new OrbitControls(camera);
-    controls.target = scene.getObjectByName('Card').position;
+    controls.target = card.position.clone();
+
     controls.maxPolarAngle = 0.9 * Math.PI / 2;
     controls.update();
 
@@ -44,6 +62,7 @@ loader.load('data/app.json', object => {
       requestAnimationFrame(animate);
       renderer.render(scene, camera);
     }
+
     window.addEventListener('resize', setAspect, false);
     setAspect();
     animate();
