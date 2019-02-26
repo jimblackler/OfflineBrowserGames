bf4e5e415424a4f73a905cd30c4b349e200865d0
Jim Blackler <jimblackler@google.com>
2019-02-26 11:09:32 +0000

    Cards are cloned.

--- war/v2/scripts/v2Renderer.js
+++ war/v2/scripts/v2Renderer.js
@@ -4,9 +4,14 @@ import {OrbitControls} from "../threejs/controls/OrbitControls.js";
 import {PCFSoftShadowMap} from "../threejs/src/constants.js";
 import {Scene} from "../threejs/src/scenes/Scene.js";
 import {Vector2} from "../threejs/src/math/Vector2.js";
+import {Vector3} from "../threejs/src/math/Vector3.js";
 import {WebGLRenderer} from "../threejs/src/renderers/WebGLRenderer.js";
 
+const TEXTURE_WIDTH = 1339;
+const TEXTURE_HEIGHT = 900;
+
 export class V2Renderer extends BaseRenderer {
+
   constructor(container) {
     super();
 
@@ -24,22 +29,23 @@ export class V2Renderer extends BaseRenderer {
     renderer.gammaInput = true;
     renderer.gammaOutput = true;
 
-    const TEXTURE_WIDTH = 1339;
-    const TEXTURE_HEIGHT = 900;
-
     const loader = new ObjectLoader();
     loader.load('data/app.json', object => {
       scene.add(object);
-      const card = scene.getObjectByName('Card');
+      const originalCard = scene.getObjectByName('Card');
+      this.paintCard(originalCard.getObjectByName('Back').material.map, 4, 0);
+      const card = originalCard.clone();
+
       const front = card.getObjectByName('Front');
+      front.material = front.material.clone();
+      front.material.map = front.material.map.clone();
+      this.paintCard(front.material.map, 3, 12);
 
-      const suit = 3;
-      const type = 12;
+      card.position.x = 10;
+      card.position.z = 10;
+      scene.add(card);
 
-      const map = front.material.map;
-      map.repeat = new Vector2(this.CARD_WIDTH / TEXTURE_WIDTH, this.CARD_HEIGHT / TEXTURE_HEIGHT);
-      map.offset = new Vector2((this.CARD_WIDTH * type) / TEXTURE_WIDTH,
-          1 - (this.CARD_HEIGHT * suit + this.CARD_HEIGHT) / TEXTURE_HEIGHT);
+      originalCard.visible = false;
 
       loader.load('data/camera.json', camera => {
         scene.add(camera);
@@ -64,5 +70,12 @@ export class V2Renderer extends BaseRenderer {
         animate();
       });
     });
+  }
+
+  paintCard(map, suit, type) {
+    map.repeat = new Vector2(this.CARD_WIDTH / TEXTURE_WIDTH, this.CARD_HEIGHT / TEXTURE_HEIGHT);
+    map.offset = new Vector2((this.CARD_WIDTH * type) / TEXTURE_WIDTH,
+        1 - (this.CARD_HEIGHT * suit + this.CARD_HEIGHT) / TEXTURE_HEIGHT);
+    map.needsUpdate = true;
   }
 }
