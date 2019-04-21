1b81a110b855f9de4e3dcf0f952edaf951a023bc
Jim Blackler <jimblackler@google.com>
2019-04-22 00:32:08 +0100

    Work on the v2 version.

--- war/v2/scripts/v2Renderer.js
+++ war/v2/scripts/v2Renderer.js
@@ -1,8 +1,8 @@
 import {PerspectiveCamera} from '../threejs/src/cameras/PerspectiveCamera.js';
 import {ObjectLoader} from "../threejs/src/loaders/ObjectLoader.js";
-import {Object3D} from '../threejs/src/core/Object3D.js';
 import {OrbitControls} from "../threejs/controls/OrbitControls.js";
 import {PCFSoftShadowMap} from "../threejs/src/constants.js";
+import {Rules} from "../../commonScripts/rules.js";
 import {Scene} from "../threejs/src/scenes/Scene.js";
 import {Vector2} from "../threejs/src/math/Vector2.js";
 import {Vector3} from "../threejs/src/math/Vector3.js";
@@ -17,6 +17,10 @@ const CARD_HEIGHT = 143;
 export class V2Renderer {
 
   constructor(container) {
+    this.cardObjects = [];
+    this.cardVPos = []; // needed??
+
+
     const renderer = new WebGLRenderer({antialias: true});
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -33,32 +37,38 @@ export class V2Renderer {
 
   init() {
     const scene = new Scene();
+    this.scene = scene;
     const loader = new ObjectLoader();
     return new Promise((resolve, reject) => {
       loader.load('data/app.json', object => {
         scene.add(object);
         const originalCard = scene.getObjectByName('Card');
-        const back = originalCard.getObjectByName('Back');
-        this.paintCard(back.material.map, 4, 0);
-        const card = originalCard.clone();
-
-        const front = card.getObjectByName('Front');
-        front.material = front.material.clone();
-        front.material.map = front.material.map.clone();
-        this.paintCard(front.material.map, 3, 12);
-
-        card.position.x = 10;
-        card.position.z = 10;
-        scene.add(card);
 
         originalCard.visible = false;
+        const back = originalCard.getObjectByName('Back');
+        this.paintCard(back.material.map, 4, 0);
 
-        loader.load('data/camera.json', objecct => {
-          const camera = object;
+        for (let cardNumber = 0; cardNumber !== Rules.NUMBER_CARDS; cardNumber++) {
+          const card = originalCard.clone();
+
+          this.cardObjects[cardNumber] = card;
+          this.cardVPos[cardNumber] = 0;
+
+          const front = card.getObjectByName('Front');
+          front.material = front.material.clone();
+          front.material.map = front.material.map.clone();
+          const suit = Rules.getSuit(cardNumber);
+          const type = Rules.getType(cardNumber);
+          this.paintCard(front.material.map, suit, type);
+
+          card.visible = true;
+          scene.add(card);
+        }
+
+        loader.load('data/camera.json', object => {
+          const camera = object || {};
           scene.add(camera);
           const controls = new OrbitControls(camera);
-          controls.target = card.position.clone();
-
           controls.update();
 
           const setAspect = () => {
@@ -102,6 +112,7 @@ export class V2Renderer {
   }
 
   faceUp(cardNumber) {
+
   }
 
   setClick(element, clickFunction) {
@@ -117,11 +128,13 @@ export class V2Renderer {
   }
 
   getCardPosition(cardNumber) {
-    return [0, 0, 0];
+    const card = this.cardObjects[cardNumber];
+    return [card.position.x * 10, card.position.z * 10, 0];
   }
 
   positionCard(cardNumber, x, y, v) {
+    const card = this.cardObjects[cardNumber];
+    card.position.x = x / 10;
+    card.position.z = y / 10;
   }
-
-
 }
