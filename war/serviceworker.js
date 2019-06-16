a8c41b4cd37eeb8b35c4f8a9d53471bc51d5133c
Jim Blackler <jimblackler@google.com>
2019-06-16 11:34:55 +0100

    Stop Service Working caching itself and its installer.

--- war/serviceworker.js
+++ war/serviceworker.js
@@ -1,10 +1,20 @@
-
-const fetchInit = {cache: 'reload'};
+const fetchInit = {cache: 'no-cache'};
 
 addEventListener('fetch', event => {
 
+  if (event.request.url.endsWith("/serviceworker.js")) {
+    console.log("Skipping for serviceworker");
+    return;
+  }
+
+  if (event.request.url.endsWith("/installServiceWorker.js")) {
+    console.log("Skipping for serviceworker installer");
+    return;
+  }
+
   event.respondWith(
-      caches.open('static-025').then(cache => {
+
+      caches.open('static-048').then(cache => {
         return cache.match(event.request).then(response => {
           if (response) {
             console.log(`${event.request.url} cached`);
@@ -12,10 +22,15 @@ addEventListener('fetch', event => {
           }
           console.log(`${event.request.url} NOT cached`);
           return fetch(event.request, fetchInit).then(response => {
-                cache.put(event.request, response.clone());
-                return response;
-              });
+            cache.put(event.request, response.clone());
+            return response;
+          });
         });
       })
   );
+});
+
+addEventListener('install', event => {
+  console.log("Installing - skipping waiting");
+  self.skipWaiting();
 });
