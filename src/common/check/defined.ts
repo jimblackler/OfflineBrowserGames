a31fd8096c71d985f75f31c07331d999fb00429e
Jim Blackler <jimblackler@gmail.com>
2026-06-29 12:06:21 +0100

    Version of assertDefined that will show a warning if the type ought not ever to be undefined.

--- src/common/check/defined.ts
+++ src/common/check/defined.ts
@@ -1,6 +1,8 @@
-export function assertDefined<T>(object: T | undefined) {
+export function assertDefined<T>(object: undefined extends T ? T : never) {
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
   if (object === undefined) {
     throw new Error();
   }
-  return object;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion
+  return object as T extends undefined ? never : T;
 }
